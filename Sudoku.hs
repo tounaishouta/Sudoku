7a73689cc373f37d9095696724365d800fc1f902
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-04-29 08:26:27 +0900

    refactor
    modified:   Sudoku.hs

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,18 +1,21 @@
-module Main where
+module Sudoku where
 
-import Control.Monad      (foldM)
-import Data.Array.Unboxed (Array, UArray, array, range, (!), (//))
-import Data.Ix            (Ix)
-import Data.List          (elemIndex, intercalate)
-import Data.Maybe         (isJust)
+import Control.Applicative ((<$>))
+import Control.Monad
+import Data.Array.Unboxed  (Array, UArray, array, range, (!), (//))
+import Data.Ix             (Ix)
+import Data.Maybe          (isJust)
+import Data.List           (elemIndex, intercalate)
 
 main :: IO ()
 main = interact (intercalate "\n" . map show . solve . read)
 
-data V = GRD | ROW | COL | BOX deriving (Bounded, Enum, Eq, Ix, Ord)
+data Sudoku = Sudoku
+    { definite :: Array (V, I, I) (Maybe I)
+    , possible :: UArray (I, I, I) Bool
+    } deriving (Eq)
 
-vs :: [V]
-vs = [minBound .. maxBound]
+data V = GRD | ROW | COL | BOX deriving (Bounded, Ix, Eq, Ord)
 
 m :: Int
 m = 3
@@ -20,111 +23,89 @@ m = 3
 n :: Int
 n = m * m
 
-type I = Int
+newtype I = I { unI :: Int } deriving (Ix, Eq, Ord)
 
-iMin :: I
-iMin = 0
+instance Bounded I where
+    minBound = I 0
+    maxBound = I (n - 1)
 
-iMax :: I
-iMax = n - 1
-
-is :: [I]
-is = [iMin .. iMax]
-
-type C = (I, I, I)
-
-cMin :: C
-cMin = (iMin, iMin, iMin)
-
-cMax :: C
-cMax = (iMax, iMax, iMax)
-
-cs :: [C]
-cs = [ (i, j, k) | i <- is , j <- is , k <- is ]
-
-toC :: (V, I, I, I) -> C
-toC (GRD, i, j, k) = (i, j, k)
-toC (ROW, k, i, j) = (i, j, k)
-toC (COL, k, j, i) = (i, j, k)
-toC (BOX, k, p, q) = (i, j, k) where
-    i = p `div` m * m + q `div` m
-    j = p `mod` m * m + q `mod` m
-
-fromC :: C -> [(V, I, I, I)]
-fromC (i, j, k) =
+fromGRD :: (I, I, I) -> [(V, I, I, I)]
+fromGRD (i, j, k) =
     [ (GRD, i, j, k)
     , (ROW, k, i, j)
     , (COL, k, j, i)
     , (BOX, k, p, q)
     ] where
-        p = i `div` m * m + j `div` m
-        q = i `mod` m * m + j `mod` m
+        p = I (unI i `div` m * m + unI j `div` m)
+        q = I (unI i `mod` m * m + unI j `mod` m)
 
-chars :: [Char]
-chars = ['1' .. '9']
+toGRD :: (V, I, I, I) -> (I, I, I)
+toGRD (GRD, i, j, k) = (i, j, k)
+toGRD (ROW, k, i, j) = (i, j, k)
+toGRD (COL, k, j, i) = (i, j, k)
+toGRD (BOX, k, p, q) = (i, j, k) where
+    i = I (unI p `div` m * m + unI q `div` m)
+    j = I (unI p `mod` m * m + unI q `mod` m)
 
-data Sudoku = Sudoku
-    { definite :: Array (V, I, I) (Maybe I)
-    , possible :: UArray C Bool
-    } deriving Eq
-
-instance Read Sudoku where
-    readsPrec _ input = [(s0, unlines (drop n lns))] where
-        s0 = foldl aux empty [ (i, j) | i <- is , j <- is ]
-        aux s (i, j) = case elemIndex (lns !! i !! j) chars of
-            Just k  -> fill s (toC (GRD, i, j, k))
-            Nothing -> s
-        lns = lines input
-
-instance Show Sudoku where
-    show s = unlines [ [ toChar (definite s ! (GRD, i, j)) | j <- is ] | i <- is ] where
-        toChar (Just k) = chars !! k
-        toChar Nothing  = '.'
+whole :: (Bounded a, Ix a) => [a]
+whole = range (minBound, maxBound)
 
 empty :: Sudoku
 empty = Sudoku { definite = d , possible = p } where
-    d = constArray ((minBound, iMin, iMin), (maxBound, iMax, iMax)) Nothing
-    p = constArray (cMin, cMax) True
-    constArray bds val = array bds [ (x, val) | x <- range bds ]
+    d = constArray Nothing
+    p = constArray True
+    constArray y = array (minBound, maxBound) [ (x, y) | x <- whole ]
 
-isComplete :: Sudoku -> Bool
-isComplete s = and [ isJust (definite s ! (v, i, j)) | v <- vs , i <- is , j <- is ]
+isCompleted :: Sudoku -> Bool
+isCompleted s = and [ isJust (definite s ! vij) | vij <- whole ]
 
-fill :: Sudoku -> C -> Sudoku
-fill s c
-    | possible s ! c
-        = s { definite = d , possible = p }
-    | otherwise
-        = error "cannot fill here"
-    where
-        d = definite s // [ ((v, i, j), Just k) | (v, i, j, k) <- fromC c ]
-        p = possible s // [ (toC (v, i, j, k), False) | (v, i, j, _) <- fromC c , k <- is ]
+fill :: Sudoku -> (I, I, I) -> Sudoku
+fill s g = s { definite = d , possible = p } where
+    d = definite s // [ ((v, i, j), Just k) | (v, i, j, k) <- fromGRD g ]
+    p = possible s // [ (toGRD (v, i, j, k), False) | (v, i, j, _) <- fromGRD g , k <- whole ]
 
-erase :: Sudoku -> C -> Sudoku
-erase s c = s { possible = possible s // [(c, False)] }
+erase :: Sudoku -> (I, I, I) -> Sudoku
+erase s g = s { possible = p } where
+    p = possible s // [(g, False)]
 
 reduce :: Sudoku -> Maybe Sudoku
-reduce s0 = foldM aux s0 [ (v, i, j) | v <- vs , i <- is , j <- is ] where
+reduce s0 = foldM aux s0 (whole :: [(V, I, I)]) where
     aux s vij @ (v, i, j)
         | isJust (definite s ! vij)
             = Just s
         | otherwise
-            = case [ k | k <- is , possible s ! toC (v, i, j, k) ] of
+            = case [ k | k <- whole , possible s ! toGRD (v, i, j, k) ] of
                 []  -> Nothing
-                [k] -> Just (fill s (toC (v, i, j, k)))
+                [k] -> Just (fill s (toGRD (v, i, j, k)))
                 _   -> Just s
 
 solve :: Sudoku -> [Sudoku]
 solve s
-    | isComplete s
+    | isCompleted s
         = [s]
     | otherwise
         = case reduce s of
             Just s'
                 | s == s'
-                    -> solve (fill s c) ++ solve (erase s c)
+                    -> solve (fill s g0) ++ solve (erase s g0)
                 | otherwise
                     -> solve s'
-                where c = head (filter (possible s !) cs)
+                where g0 = head [ g | g <- whole , possible s ! g ]
             Nothing
                 -> []
+
+chars :: String -- [Char]
+chars = ['1' .. '9']
+
+instance Read Sudoku where
+    readsPrec _ input = [(s0, unlines (drop n lns))] where
+        s0 = foldl aux empty (whole :: [(I, I)])
+        aux s (i, j) = case I <$> elemIndex (lns !! unI i !! unI j) chars of
+            Just k  -> fill s (i, j, k)
+            Nothing -> s
+        lns = lines input
+
+instance Show Sudoku where
+    show s = unlines [ [ toChar (definite s ! (GRD, i, j)) | j <- whole ] | i <- whole ] where
+        toChar (Just k) = chars !! unI k
+        toChar Nothing  = '.'
