625c2847a37350536afb32fe7596acf47fde70cc
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-05-10 00:38:59 +0900

    change implementation: Board = Array (V, I, I) State

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,7 +1,9 @@
+module Main where
+
 import Control.Monad (foldM)
-import Data.Array    (Array, accum, array, (!), (//))
+import Data.Array    (Array, accum, array, (!))
 import Data.Ix       (Ix, range)
-import Data.List     (intercalate, delete)
+import Data.List     (delete, intercalate)
 
 main :: IO ()
 main = interact $ intercalate "\n" . map showBoard . solve . readBoard
@@ -23,11 +25,9 @@ data V = GRD | ROW | COL | BOX deriving (Bounded, Eq, Ix, Ord)
 whole :: (Bounded a, Ix a) => [a]
 whole = range (minBound, maxBound)
 
-data Board = Board
-    { definite :: Array (V, I, I) (Maybe I)
-    , possible :: Array (V, I, I) [I]
-    , progress :: Int
-    }
+data State = Definite I | Indefinite [I]
+
+type Board = Array (V, I, I) State
 
 solve :: Board -> [Board]
 solve b
@@ -37,36 +37,41 @@ solve b
         = case reduce b of
             Nothing -> []
             Just b'
-                | progress b < progress b'
+                | filled b < filled b'
                     -> solve b'
                 | otherwise
-                    -> solve (set b vijk) ++ solve (unset b vijk)
-                where vijk = head [ (vij, k) | vij <- whole , k <- possible b ! vij ]
+                    -> concat [ solve $ set b (vij, k) | k <- possibles b vij ]
+                where vij = head $ filter (not . isDefinite . (b !)) whole
 
 isComplete :: Board -> Bool
-isComplete b = progress b == n * n
+isComplete b = filled b == n * n
+
+filled :: Board -> Int
+filled b = length [ (i, j) | (i, j) <- whole , isDefinite $ b ! (GRD, i, j) ]
+
+isDefinite :: State -> Bool
+isDefinite (Definite   _) = True
+isDefinite (Indefinite _) = False
 
 reduce :: Board -> Maybe Board
 reduce b = foldM check b whole
 
 check :: Board -> (V, I, I) -> Maybe Board
-check b vij = case (definite b ! vij, possible b ! vij) of
-    (Nothing, [])  -> Nothing
-    (Nothing, [k]) -> Just $ set b (vij, k)
+check b vij = case b ! vij of
+    Indefinite []  -> Nothing
+    Indefinite [k] -> Just $ set b (vij, k)
     _              -> Just b
 
 set :: Board -> ((V, I, I), I) -> Board
-set b vijk = b
-    { definite = definite b // [ (vij, Just k) | (vij, k) <- convert vijk ]
-    , possible = accum (flip delete) (possible b) diff
-    , progress = progress b + 1
-    } where
-        diff = concat [ convert (vij, k) | (vij, _) <- convert vijk , k <- possible b ! vij ]
+set b vijk = accum determine (accum remove b diff) (convert vijk) where
+    diff = concat [ convert (vij, k') | (vij, k) <- convert vijk , k' <- possibles b vij , k /= k' ]
 
-unset :: Board -> ((V, I, I), I) -> Board
-unset b vijk = b { possible = accum (flip delete) (possible b) (convert vijk) }
+possibles :: Board -> (V, I, I) -> [I]
+possibles b vij = case b ! vij of
+    Definite    _ -> error "possibles: already definite"
+    Indefinite ks -> ks
 
-convert :: ((V, I, I) , I) -> [((V, I, I), I)]
+convert :: ((V, I, I), I) -> [((V, I, I), I)]
 convert ((GRD, i, j), k) =
     [ ((GRD, i, j), k)
     , ((ROW, k, i), j)
@@ -75,22 +80,38 @@ convert ((GRD, i, j), k) =
     ] where
         p = I $ unI i `div` m * m + unI j `div` m
         q = I $ unI i `mod` m * m + unI j `mod` m
+
 convert ((ROW, k, i), j) = convert ((GRD, i, j), k)
 convert ((COL, k, j), i) = convert ((GRD, i, j), k)
 convert ((BOX, k, p), q) = convert ((GRD, i, j), k) where
     i = I $ unI p `div` m * m + unI q `div` m
     j = I $ unI p `mod` m * m + unI q `mod` m
 
+determine :: State -> I -> State
+determine (Indefinite ks) k
+    | k `elem` ks
+        = Definite k
+    | otherwise
+        = error "determine: impossible choice"
+determine (Definite _) _
+    = error "determine: already definite"
+
+remove :: State -> I -> State
+remove (Indefinite ks) k = Indefinite $ delete k ks
+remove (Definite    _) _ = error "remove: already definite"
+
 showBoard :: Board -> String
-showBoard b = unlines [ [ maybe '.' showI $ definite b ! (GRD, i, j) | j <- whole ] | i <- whole ]
+showBoard b = unlines [ [ aux i j | j <- whole ] | i <- whole ] where
+    aux i j = case b ! (GRD, i, j) of
+        Definite   k -> showI k
+        Indefinite _ -> '.'
 
 readBoard :: String -> Board
 readBoard s = foldl aux empty whole where
     aux b (i, j) = case readI $ lines s !! unI i !! unI j of
         Nothing -> b
         Just k  -> set b ((GRD, i, j), k)
-    empty = Board { definite = constArray Nothing , possible = constArray whole , progress = 0 }
-    constArray y = array (minBound, maxBound) [ (x, y) | x <- whole ]
+    empty = array (minBound, maxBound) [ (vij, Indefinite whole) | vij <- whole ]
 
 showI :: I -> Char
 showI = (['1' .. '9'] !!) . unI
