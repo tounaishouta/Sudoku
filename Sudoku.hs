7fdb47ca80c5453e217824189b2ef2d021ecaacf
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-06-11 18:22:19 +0900

    rewrite Sudoku.hs

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,100 +1,96 @@
 import Control.Monad
 import Data.Array.Unboxed
 import Data.List
-import Data.Tuple
 
 main :: IO ()
-main = interact $ unlines . map showGrid . liftMaybe solve . readGrid
+main = interact $ unlines . map showGrid . solve <=< readGrid
 
 type Digit = Char
 
-digitss :: [[Digit]]
-digitss = ["123", "456", "789"]
-
-digits :: [Digit]
-digits = concat digitss
-
-type Coord = (Digit, Digit, Digit)
-
-boundsOfCoord :: (Coord, Coord)
-boundsOfCoord = ((dMin, dMin, dMin), (dMax, dMax, dMax)) where
-    dMin = minimum digits
-    dMax = maximum digits
+type Coord = (Digit , Digit , Digit)
 
 type Block = Int
 
-coords :: Array Block [Coord]
-coords = listArray (1, length blocks) blocks where
-    blocks  = grids ++ rows ++ columns ++ boxes
-    grids   = [ triples [i] [j] digits | i <- digits , j <- digits ]
-    rows    = [ triples [i] digits [d] | i <- digits , d <- digits ]
-    columns = [ triples digits [j] [d] | j <- digits , d <- digits ]
-    boxes   = [ triples is js [d] | is <- digitss , js <- digitss , d <- digits ]
-    triples is js ds = [ (i, j, d) | i <- is , j <- js , d <- ds ]
-
-owners :: Array Coord [Block]
-owners = accumArray append [] boundsOfCoord [ (c, b) | (b, cs) <- assocs coords , c <- cs ]
-    where append cs c = cs ++ [c]
-
-others :: Array Coord [Coord]
-others = array boundsOfCoord [ (c, aux c bs) | (c, bs) <- assocs owners ]
-    where aux c bs = delete c $ nub $ concat [ coords ! b | b <- bs ]
-
-boundsOfBlock :: (Block, Block)
-boundsOfBlock = bounds coords
-
-type Grid = (UArray Coord Bool, UArray Block Int)
+type Grid = (UArray Coord Bool , UArray Block Int)
 
 defined :: Int
 defined = maxBound
 
-showGrid :: Grid -> String
-showGrid (p, _) = unlines [ [ aux i j | j <- digits ] | i <- digits ] where
-    aux i j = case [ d | d <- digits , p ! (i, j, d) ] of
-        [d] -> d
-        _   -> '.'
+groups :: [[Digit]]
+groups = ["123" , "456" , "789"]
 
-liftMaybe :: MonadPlus m => (a -> m b) -> Maybe a -> m b
-liftMaybe = maybe mzero
+digits :: [Digit]
+digits = concat groups
+
+boundsOfCoord :: (Coord , Coord)
+boundsOfCoord = ((dMin , dMin , dMin) , (dMax , dMax , dMax)) where
+    dMin = minimum digits
+    dMax = maximum digits
+
+boundsOfBlock :: (Block , Block)
+boundsOfBlock = bounds members
+
+members :: Array Block [Coord]
+members = listArray (1 , length list) list where
+    list    = numbers ++ rows ++ columns ++ boxes
+    numbers = [ triples [i] [j] digits | i <- digits , j <- digits ]
+    rows    = [ triples [i] digits [d] | i <- digits , d <- digits ]
+    columns = [ triples digits [j] [d] | j <- digits , d <- digits ]
+    boxes   = [ triples is js [d] | is <- groups , js <- groups , d <- digits ]
+    triples is js ds = [ (i , j , d) | i <- is , j <- js , d <- ds ]
+
+owners :: Array Coord [Block]
+owners = accumArray add [] boundsOfCoord cbs where
+    add cs c = cs ++ [c]
+    cbs = [ (c , b) | (b , cs) <- assocs members , c <- cs ]
+
+antis :: Array Coord [Coord]
+antis = array boundsOfCoord [ (c , aux c bs) | (c , bs) <- assocs owners ] where
+    aux c bs = delete c $ nub $ concat [ members ! b | b <- bs ]
 
 solve :: MonadPlus m => Grid -> m Grid
-solve g @ (_, o)
+solve g @ (_ , o)
     | n == defined
         = return g
     | otherwise
-        = msum [ liftMaybe solve $ assign g c | c <- coords ! b ]
-    where (n, b) = minimum $ map swap $ assocs o
+        = msum [ solve =<< assign g c | c <- members ! b ]
+    where (n , b) = minimum [ (n' , b') | (b' , n') <- assocs o ]
 
-readGrid :: String -> Maybe Grid
-readGrid = foldM aux emptyGrid . zip ijs . concat . words where
-    ijs = [ (i, j) | i <- digits , j <- digits ]
-    aux g ((i, j), d)
-        | d `elem` digits
-            = assign g (i, j, d)
-        | otherwise
-            = return g
-
-assign :: Grid -> Coord -> Maybe Grid
-assign (p, o) c
+assign :: MonadPlus m => Grid -> Coord -> m Grid
+assign (p , o) c
     | p ! c
-        = foldM check (p', o') bs
+        = foldM check (p' , o') bs
     | otherwise
         = mzero
     where
-        cs = [ c' | c' <- others ! c , p ! c' ]
+        cs = [ c' | c' <- antis ! c , p ! c' ]
         bs = [ b | c' <- cs , b <- owners ! c' ]
-        p' = p // [ (c', False) | c' <- cs ]
-        o' = accum (-) o [ (b, 1) | b <- bs ] // [ (b, defined) | b <- owners ! c ]
+        p' = p // [ (c' , False) | c' <- cs ]
+        o' = accum (-) o [ (b , 1) | b <- bs ] // [ (b , defined) | b <- owners ! c ]
 
-check :: Grid -> Block -> Maybe Grid
-check g @ (p, o) b = case o ! b of
+check :: MonadPlus m => Grid -> Block -> m Grid
+check g @ (p , o) b = case o ! b of
     0 -> mzero
     1 -> assign g c
     _ -> return g
-    where c = head $ filter (p !) $ coords ! b
+    where c = head [ c' | c' <- members ! b , p ! c' ]
+
+showGrid :: Grid -> String
+showGrid (p , _) = unlines [ [ aux i j | j <- digits ] | i <- digits ] where
+    aux i j = case [ d | d <- digits , p ! (i , j , d) ] of
+        [d] -> d
+        _   -> '.'
+
+readGrid :: MonadPlus m => String -> m Grid
+readGrid = foldM aux emptyGrid . zip ijs . concat . words where
+    ijs = [ (i , j) | i <- digits , j <- digits ]
+    aux g ((i , j) , d)
+        | d `elem` digits
+            = assign g (i , j , d)
+        | otherwise
+            = return g
 
 emptyGrid :: Grid
-emptyGrid = (p, o) where
-    p = constArray boundsOfCoord True
-    o = constArray boundsOfBlock $ length digits
-    constArray bds v = array bds [ (k, v) | k <- range bds ]
+emptyGrid = (p , o) where
+    p = array boundsOfCoord [ (c , True) | c <- range boundsOfCoord ]
+    o = array boundsOfBlock [ (b , length digits) | b <- range boundsOfBlock ]
