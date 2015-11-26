866681a2865075981edf3a4e3013cf00769f8b0a
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-11-26 16:22:58 +0900

    rewrite solver.go

--- solver.go
+++ solver.go
@@ -8,22 +8,31 @@ import (
 
 func main() {
 	for scanner := bufio.NewScanner(os.Stdin); scanner.Scan(); {
-		input := scanner.Text()
-		if s, ok := NewSudoku(input); ok {
+		if s, ok := NewSudoku(scanner.Text()); ok {
 			if s, ok := s.Solve(); ok {
 				fmt.Println(s)
 				continue
 			}
-			fmt.Println("fail to Solve")
-			continue
 		}
-		fmt.Println("fail to NewSudoku")
-		continue
+		fmt.Println("failed to solve")
 	}
+}
+
+type sudoku struct {
+	admits [nCoord]bool
+	choice [nBlock]int
 }
 
 type coord int
 type block int
+
+const (
+	size    = 3
+	nDigit  = size * size
+	nCoord  = nDigit * nDigit * nDigit
+	nBlock  = nView * nDigit * nDigit
+	defined = 999
+)
 
 const (
 	grd = iota
@@ -32,56 +41,6 @@ const (
 	box
 	nView
 )
-
-const (
-	size   = 3
-	nDigit = size * size
-	nCoord = nDigit * nDigit * nDigit
-	nBlock = nView * nDigit * nDigit
-)
-
-const defined = 99
-
-type sudoku struct {
-	admits [nCoord]bool
-	choice [nBlock]int
-}
-
-var (
-	parents  [nCoord][]block
-	children [nBlock][]coord
-	siblings [nCoord][]coord
-)
-
-func init() {
-	for i := 0; i < nDigit; i++ {
-		for j := 0; j < nDigit; j++ {
-			p := i/size*size + j/size
-			for k := 0; k < nDigit; k++ {
-				c := toCoord(i, j, k)
-				parents[c] = []block{toBlock(grd, i, j), toBlock(row, i, k), toBlock(col, j, k), toBlock(box, p, k)}
-			}
-		}
-	}
-	for c := 0; c < nCoord; c++ {
-		for _, b := range parents[c] {
-			children[b] = append(children[b], coord(c))
-		}
-	}
-	for c := 0; c < nCoord; c++ {
-		cs := make(map[coord]bool)
-		for _, b := range parents[c] {
-			for _, cc := range children[b] {
-				cs[cc] = true
-			}
-		}
-		for cc, _ := range cs {
-			if cc != coord(c) {
-				siblings[c] = append(siblings[c], cc)
-			}
-		}
-	}
-}
 
 func toCoord(i, j, k int) coord {
 	return coord((i*nDigit+j)*nDigit + k)
@@ -105,8 +64,8 @@ func NewSudoku(input string) (*sudoku, bool) {
 		}
 		i, j, k := p/nDigit, p%nDigit, int(r-'1')
 		if 0 <= k && k < nDigit {
-			if ss, ok := s.assign(toCoord(i, j, k)); ok {
-				s = ss
+			if s1, ok := s.assign(toCoord(i, j, k)); ok {
+				s = s1
 			} else {
 				return nil, false
 			}
@@ -115,47 +74,23 @@ func NewSudoku(input string) (*sudoku, bool) {
 	return s, true
 }
 
-func (s *sudoku) String() string {
-	output := make([]rune, 0, nDigit*nDigit)
-	for i := 0; i < nDigit; i++ {
-		for j := 0; j < nDigit; j++ {
-			var k int
-			n := 0
-			for kk := 0; kk < nDigit; kk++ {
-				if s.admits[toCoord(i, j, kk)] {
-					k = kk
-					n++
-				}
-			}
-			switch n {
-			case 0:
-				output = append(output, 'x')
-			case 1:
-				output = append(output, rune('1'+k))
-			default:
-				output = append(output, '.')
-			}
-		}
-	}
-	return string(output)
-}
-
 func (s *sudoku) Solve() (*sudoku, bool) {
-	var bmin block
+	var b0 block
 	min := defined
 	for b := 0; b < nBlock; b++ {
 		if s.choice[b] < min {
-			min, bmin = s.choice[b], block(b)
+			min = s.choice[b]
+			b0 = block(b)
 		}
 	}
 	if min == defined {
 		return s, true
 	}
-	for _, c := range children[bmin] {
+	for _, c := range children[b0] {
 		if s.admits[c] {
-			if ss, ok := s.clone().assign(c); ok {
-				if sss, ok := ss.Solve(); ok {
-					return sss, true
+			if s1, ok := s.clone().assign(c); ok {
+				if s1, ok := s1.Solve(); ok {
+					return s1, true
 				}
 			}
 		}
@@ -163,37 +98,46 @@ func (s *sudoku) Solve() (*sudoku, bool) {
 	return nil, false
 }
 
-func (s *sudoku) clone() *sudoku {
-	ss := new(sudoku)
-	for c := 0; c < nCoord; c++ {
-		ss.admits[c] = s.admits[c]
+func (s *sudoku) String() string {
+	output := make([]rune, 0, nDigit*nDigit)
+	for i := 0; i < nDigit; i++ {
+		for j := 0; j < nDigit; j++ {
+			if s.choice[toBlock(grd, i, j)] == defined {
+				var k0 int
+				for k := 0; k < nDigit; k++ {
+					if s.admits[toCoord(i, j, k)] {
+						k0 = k
+					}
+				}
+				output = append(output, rune('1'+k0))
+			} else {
+				output = append(output, '.')
+			}
+		}
 	}
-	for b := 0; b < nBlock; b++ {
-		ss.choice[b] = s.choice[b]
-	}
-	return ss
+	return string(output)
 }
 
-func (s *sudoku) assign(c coord) (*sudoku, bool) {
-	if !s.admits[c] {
+func (s *sudoku) assign(c0 coord) (*sudoku, bool) {
+	if !s.admits[c0] {
 		return nil, false
 	}
 	bs := make([]block, 0)
-	for _, cc := range siblings[c] {
-		if s.admits[cc] {
-			s.admits[cc] = false
-			for _, b := range parents[cc] {
+	for _, c := range siblings[c0] {
+		if s.admits[c] {
+			s.admits[c] = false
+			for _, b := range parents[c] {
 				s.choice[b]--
 				bs = append(bs, b)
 			}
 		}
 	}
-	for _, b := range parents[c] {
+	for _, b := range parents[c0] {
 		s.choice[b] = defined
 	}
 	for _, b := range bs {
-		if ss, ok := s.check(b); ok {
-			s = ss
+		if s1, ok := s.check(b); ok {
+			s = s1
 		} else {
 			return nil, false
 		}
@@ -206,14 +150,61 @@ func (s *sudoku) check(b block) (*sudoku, bool) {
 	case 0:
 		return nil, false
 	case 1:
-		var c coord
-		for _, c = range children[b] {
+		var c0 coord
+		for _, c := range children[b] {
 			if s.admits[c] {
-				break
+				c0 = c
 			}
 		}
-		return s.assign(c)
+		return s.assign(c0)
 	default:
 		return s, true
+	}
+}
+
+func (s0 *sudoku) clone() *sudoku {
+	s := new(sudoku)
+	for c := 0; c < nCoord; c++ {
+		s.admits[c] = s0.admits[c]
+	}
+	for b := 0; b < nBlock; b++ {
+		s.choice[b] = s0.choice[b]
+	}
+	return s
+}
+
+var (
+	parents  [nCoord][]block
+	children [nBlock][]coord
+	siblings [nCoord][]coord
+)
+
+func init() {
+	for i := 0; i < nDigit; i++ {
+		for j := 0; j < nDigit; j++ {
+			p := i/size*size + j/size
+			for k := 0; k < nDigit; k++ {
+				parents[toCoord(i, j, k)] = []block{
+					toBlock(grd, i, j),
+					toBlock(row, i, k),
+					toBlock(col, j, k),
+					toBlock(box, p, k),
+				}
+			}
+		}
+	}
+	for c := 0; c < nCoord; c++ {
+		for _, b := range parents[c] {
+			children[b] = append(children[b], coord(c))
+		}
+	}
+	for c0 := 0; c0 < nCoord; c0++ {
+		for _, b := range parents[c0] {
+			for _, c := range children[b] {
+				if c != coord(c0) {
+					siblings[c0] = append(siblings[c0], c)
+				}
+			}
+		}
 	}
 }
