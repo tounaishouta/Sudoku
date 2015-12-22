95320d825277eb651f6a4d55a6ce4ebf4f4b738a
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-12-22 12:09:19 +0900

    rewrite Sudoku.hs

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,91 +1,97 @@
-import Control.Monad
-import Data.Array.Unboxed
-import Data.List
-import Data.Tuple
+import Control.Monad      (foldM)
+import Data.Array.Unboxed (Array, Ix, UArray, accum, accumArray, array, assocs, (!), (//))
+import Data.Char          (chr, ord)
+import Data.List          (delete, nub)
+import Data.Tuple         (swap)
 
 main :: IO ()
-main = interact $ unlines . map (maybe "no solution" showSudoku . solve) . lines
+main = interact $ unlines . map (showSudoku . head . solveMaybe . readSudoku) . lines
 
-type Sudoku = (UArray Coord Bool, UArray Unit Int)
+newtype Sudoku = Sudoku (UArray Coord Bool, UArray Block Int)
 
-type Digit = Char
+solve :: Sudoku -> [Sudoku]
+solve s @ (Sudoku (_, o))
+    | m == defined = [s]
+    | otherwise    = concatMap (solveMaybe . assign s) (children ! b)
+    where (m, b) = minimum $ map swap (assocs o)
 
-digits :: [Digit]
-digits = concat blocks
+solveMaybe :: Maybe Sudoku -> [Sudoku]
+solveMaybe = maybe [] solve
 
-blocks :: [[Digit]]
-blocks = ["123", "456", "789"]
+assign :: Sudoku -> Coord -> Maybe Sudoku
+assign (Sudoku (a, o)) c
+    | a ! c     = foldM check (Sudoku (a', o')) bs
+    | otherwise = Nothing
+    where
+        a' = a // zip cs (repeat False)
+        o' = accum (-) o (zip bs (repeat 1)) // zip (parents ! c) (repeat defined)
+        cs = filter (a !) (siblings ! c)
+        bs = concatMap (parents !) cs
 
-type Coord = Int
-
-boundsCoord :: (Coord, Coord)
-boundsCoord = (minimum coords, maximum coords) where
-    coords = elems coord
-
-coord :: Array (Digit, Digit, Digit) Coord
-coord = array ((a, a, a), (b, b, b)) (zip ijks [1 .. ]) where
-    ijks = [ (i, j, k) | i <- digits, j <- digits, k <- digits ]
-    a = minimum digits
-    b = maximum digits
-
-type Unit = Int
-
-boundsUnit :: (Unit, Unit)
-boundsUnit = bounds children
-
-children :: Array Unit [Coord]
-children = listArray (1, length units) units where
-    units = grids ++ rows ++ cols ++ boxes
-    grids = [ [ coord ! (i, j, k) | k <- digits ] | i <- digits, j <- digits ]
-    rows  = [ [ coord ! (i, j, k) | j <- digits ] | i <- digits, k <- digits ]
-    cols  = [ [ coord ! (i, j, k) | i <- digits ] | j <- digits, k <- digits ]
-    boxes = [ [ coord ! (i, j, k) | i <- is, j <- js ] | is <- blocks, js <- blocks, k <- digits ]
-
-parents :: Array Coord [Unit]
-parents = accumArray (flip (:)) [] boundsCoord [ (c, u) | (u, cs) <- assocs children, c <- cs ]
-
-siblings :: Array Coord [Coord]
-siblings = array boundsCoord [ (c, delete c $ nub $ concatMap (children !) us) | (c, us) <- assocs parents ]
-
-determined :: Int
-determined = maxBound
-
-solve :: MonadPlus m => String -> m Sudoku
-solve ks = solveRec =<< foldM assign emptySudoku cs where
-    cs  = [ coord ! (i, j, k) | ((i, j), k) <- zip ijs ks, k `elem` digits ]
-    ijs = [ (i, j) | i <- digits, j <- digits ]
-
-solveRec :: MonadPlus m => Sudoku -> m Sudoku
-solveRec s @ (a, o)
-    | n == determined
-        = return s
-    | otherwise
-        = msum [ solveRec =<< assign s c | c <- children ! u, a ! c ]
-    where (n, u) = minimum $ map swap $ assocs o
-
-assign :: MonadPlus m => Sudoku -> Coord -> m Sudoku
-assign (a, o) c
-    | not $ a ! c
-        = mzero
-    | otherwise
-        = foldM check (a', o') us where
-            a' = a // [ (c', False) | c' <- cs ]
-            o' = accum (-) o [ (u, 1) | u <- us ] // [ (u, determined) | u <- parents ! c ]
-            cs = filter (a !) (siblings ! c)
-            us = concatMap (parents !) cs
-
-check :: MonadPlus m => Sudoku -> Unit -> m Sudoku
-check s @ (a, o) u = case o ! u of
-    0 -> mzero
+check :: Sudoku -> Block -> Maybe Sudoku
+check s @ (Sudoku (a, o)) b = case o ! b of
+    0 -> Nothing
     1 -> assign s c
     _ -> return s
-    where [c] = filter (a !) (children ! u)
-
-emptySudoku :: Sudoku
-emptySudoku = (accumArray const True boundsCoord [], accumArray const (length digits) boundsUnit [])
+    where [c] = filter (a !) (children ! b)
 
 showSudoku :: Sudoku -> String
-showSudoku (a, _) =  [ showGrid i j | i <- digits, j <- digits ] where
-    showGrid i j = case [ k | k <- digits, a ! (coord ! (i, j, k)) ] of
-        [k] -> k
-        _ -> '.'
+showSudoku (Sudoku (a, _)) = [ show' i j | i <- digits, j <- digits ] where
+    show' i j = case filter ((a !) . toCoord i j) digits of
+        [k] -> fromDigit k
+        _   -> '.'
+    fromDigit k = chr $ ord '1' + k
+
+readSudoku :: String -> Maybe Sudoku
+readSudoku str = foldM assign emptySudoku cs where
+    cs = [ toCoord i j (toDigit ch) | ((i, j), ch) <- zip ijs str, isDigit ch ]
+    ijs = [ (i, j) | i <- digits, j <- digits ]
+    isDigit ch = '1' <= ch && ch <= '9'
+    toDigit ch = ord ch - ord '1'
+
+emptySudoku :: Sudoku
+emptySudoku = Sudoku (accumArray const True boundsCoord [], accumArray const nDigit boundsBlock [])
+
+size :: Int
+size = 3
+
+nDigit :: Int
+nDigit = size * size
+
+digits :: [Int]
+digits = take nDigit [0 .. ]
+
+newtype Coord  = Coord Int deriving (Eq, Ord, Ix)
+
+toCoord :: Int -> Int -> Int -> Coord
+toCoord i j k = Coord $ (i * nDigit + j) * nDigit + k
+
+boundsCoord :: (Coord, Coord)
+boundsCoord = (toCoord 0 0 0, toCoord (nDigit - 1) (nDigit - 1) (nDigit - 1)) where
+
+data View = GRD | ROW | COL | BOX
+
+newtype Block  = Block Int deriving (Eq, Ord, Ix)
+
+toBlock :: View -> Int -> Int -> Block
+toBlock GRD i j = Block $ (0 * nDigit + i) * nDigit + j
+toBlock ROW i j = Block $ (1 * nDigit + i) * nDigit + j
+toBlock COL i j = Block $ (2 * nDigit + i) * nDigit + j
+toBlock BOX i j = Block $ (3 * nDigit + i) * nDigit + j
+
+boundsBlock :: (Block, Block)
+boundsBlock = (toBlock GRD 0 0, toBlock BOX (nDigit - 1) (nDigit - 1)) where
+
+defined :: Int
+defined = 0xDEF
+
+parents :: Array Coord [Block]
+parents = array boundsCoord
+    [ (toCoord i j k, [toBlock GRD i j, toBlock ROW i k, toBlock COL j k, toBlock BOX p k])
+    | i <- digits , j <- digits , let p = i `div` size * size + j `div` size , k <- digits ]
+
+children :: Array Block [Coord]
+children = accumArray (flip (:)) [] boundsBlock [ (b, c) | (c, bs) <- assocs parents, b <- bs ]
+
+siblings :: Array Coord [Coord]
+siblings = array boundsCoord [ (c, delete c . nub $ concatMap (children !) bs) | (c, bs) <- assocs parents ]
