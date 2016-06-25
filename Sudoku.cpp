decabc803117b672271ff04721dec811b51bda4d
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-06-25 12:25:37 +0900

    rewrite

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
-#include <set>
+#include <queue>
+#include <string>
 #include <vector>
 using namespace std;
 
 typedef int coord;
 typedef int block;
 
-enum view { GRD = 0, ROW = 1, COL = 2, BOX = 3 };
-const int SIZE    = 3;
-const int N_DIGIT = SIZE * SIZE;
-const int N_COORD = N_DIGIT * N_DIGIT * N_DIGIT;
-const int N_VIEW  = 4;
-const int N_BLOCK = N_VIEW * N_DIGIT * N_DIGIT;
+enum view { GRD, ROW, COL, BOX };
+
+const int VIEW    = 4;
+const int UNIT    = 3;
+const int SIZE    = UNIT * UNIT;
+const int COORD   = SIZE * SIZE * SIZE;
+const int BLOCK   = VIEW * SIZE * SIZE;
 const int DEFINED = 0xDEF;
 
-inline bool is_digit(char ch) {
-    return '1' <= ch && ch <= '9';
-}
-inline int digit_of(char ch) {
-    return ch - '1';
-}
-inline char char_of(int i) {
-    return '1' + i;
-}
+const string DIGITS = "123456789";
+
 inline coord coord_of(int i, int j, int k) {
-    return (i * N_DIGIT + j) * N_DIGIT + k;
-}
-inline block block_of(view v, int i, int j) {
-    return (v * N_DIGIT + i) * N_DIGIT + j;
+    return (i * SIZE + j) * SIZE + k;
 }
 
-vector<block> parents[N_COORD];
-vector<coord> children[N_BLOCK];
-vector<coord> siblings[N_COORD];
+inline block block_of(view v, int p, int q) {
+    return (v * SIZE + p) * SIZE + q;
+}
 
-void initialize() {
-    for (int i = 0; i < N_DIGIT; i++) {
-        for (int j = 0; j < N_DIGIT; j++) {
-            int p = i / SIZE * SIZE + j / SIZE;
-            for (int k = 0; k < N_DIGIT; k++) {
+vector<block> parents[COORD];
+vector<coord> children[BLOCK];
+
+void init() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            int p = i / UNIT * UNIT + j / UNIT;
+            for (int k = 0; k < SIZE; k++) {
                 coord c = coord_of(i, j, k);
                 parents[c].push_back(block_of(GRD, i, j));
                 parents[c].push_back(block_of(ROW, i, k));
@@ -47,149 +42,124 @@ void initialize() {
             }
         }
     }
-    for (coord c = 0; c < N_COORD; c++) {
-        for (block b : parents[c]) {
+    for (coord c = 0; c < COORD; c++)
+        for (block b : parents[c])
             children[b].push_back(c);
-        }
-    }
-    set<coord> cset;
-    for (coord c = 0; c < N_COORD; c++) {
-        cset.clear();
-        for (block b : parents[c]) {
-            for (coord cc : children[b]) {
-                cset.insert(cc);
-            }
-        }
-        cset.erase(c);
-        for (coord cc : cset) {
-            siblings[c].push_back(cc);
-        }
-    }
 }
 
-class no_solution {} NO_SOLUTION;
+class no_solution_exception {} NO_SOLUTION_EXCEPTION;
 
 class sudoku {
-    private:
-        bool admits[N_COORD];
-        int  option[N_BLOCK];
-        sudoku& assign(coord c) {
-            if (not admits[c]) {
-                throw NO_SOLUTION;
-            }
-            set<block> bset;
-            for (coord cc : siblings[c]) {
-                if (admits[cc]) {
-                    admits[cc] = false;
-                    for (block b : parents[cc]) {
-                        bset.insert(b);
-                        option[b]--;
-                    }
-                }
-            }
-            for (block b : parents[c]) {
-                option[b] = DEFINED;
-            }
-            for (block b : bset) {
-                check(b);
-            }
-            return *this;
-        }
-        sudoku& check(block b) {
-            switch (option[b]) {
-                case 0:
-                    throw NO_SOLUTION;
-                case 1:
-                    for (coord c : children[b]) {
-                        if (admits[c]) {
-                            return assign(c);
-                        }
-                    }
-                    throw "something wrong";
-                default:
-                    return *this;
-            }
-        }
+
     public:
+
+        static string solve(string input) {
+            try {
+                sudoku s;
+                return s.read(input).search().to_string();
+            }
+            catch (no_solution_exception) {
+                return "NO SOLUTION";
+            }
+        }
+
+    private:
+
+        bool admit[COORD];
+
+        int count[BLOCK];
+
         sudoku() {
-            clear();
+            for (coord c = 0; c < COORD; c++)
+                admit[c] = true;
+            for (block b = 0; b < BLOCK; b++)
+                count[b] = SIZE;
         }
-        sudoku(const sudoku& that) {
-            copy(that);
+
+        sudoku(const sudoku& s) {
+            for (coord c = 0; c < COORD; c++)
+                admit[c] = s.admit[c];
+            for (block b = 0; b < BLOCK; b++)
+                count[b] = s.count[b];
         }
-        sudoku& clear() {
-            for (coord c = 0; c < N_COORD; c++) {
-                admits[c] = true;
-            }
-            for (block b = 0; b < N_BLOCK; b++) {
-                option[b] = N_DIGIT;
-            }
-            return *this;
-        }
-        sudoku& copy(const sudoku& that) {
-            for (coord c = 0; c < N_COORD; c++) {
-                admits[c] = that.admits[c];
-            }
-            for (block b = 0; b < N_BLOCK; b++) {
-                option[b] = that.option[b];
-            }
-            return *this;
-        }
-        sudoku& read(string input) {
-            for (int p = 0, l = input.length(); p < l && p < N_DIGIT * N_DIGIT; p++) {
-                char ch = input[p];
-                if (not is_digit(ch)) {
-                    continue;
-                }
-                int i = p / N_DIGIT;
-                int j = p % N_DIGIT;
-                int k = digit_of(ch);
-                assign(coord_of(i, j, k));
-            }
-            return *this;
-        }
-        sudoku& solve() {
-            int minimum_option = DEFINED;
-            for (int o : option) {
-                if (o < minimum_option) {
-                    minimum_option = o;
+
+        sudoku& search() {
+
+            int min = DEFINED;
+            int bmin = -1;
+            for (block b = 0; b < BLOCK; b++) {
+                if (count[b] < min) {
+                    min = count[b];
+                    bmin = b;
                 }
             }
-            if (minimum_option == DEFINED) {
+
+            if (min == DEFINED)
                 return *this;
-            }
-            block b = 0;
-            while (option[b] != minimum_option) {
-                b++;
-            }
-            sudoku temp(*this);
-            for (coord c : children[b]) {
-                if (not temp.admits[c]) {
-                    continue;
-                }
-                try {
-                    return copy(temp).assign(c).solve();
-                }
-                catch (no_solution){
-                    continue;
+
+            for (coord c : children[bmin]) {
+                if (admit[c]) {
+                    try {
+                        sudoku s(*this);
+                        return s.assign(c).search();
+                    }
+                    catch (no_solution_exception) {
+                    }
                 }
             }
-            throw NO_SOLUTION;
+
+            throw NO_SOLUTION_EXCEPTION;
         }
-        string show() {
-            string output;
-            for (int i = 0; i < N_DIGIT; i++) {
-                for (int j = 0; j < N_DIGIT; j++) {
-                    if (option[block_of(GRD, i, j)] == DEFINED) {
-                        int k = 0;
-                        while (not admits[coord_of(i, j, k)]) {
-                            k++;
+
+        sudoku& read(string input) {
+            int l = min(int(input.length()), SIZE * SIZE);
+            for (int ij = 0; ij < l; ij++) {
+                int k = DIGITS.find(input[ij]);
+                if (k != -1)
+                    assign(coord_of(ij / SIZE, ij % SIZE, k));
+            }
+            return *this;
+        }
+
+
+        sudoku& assign(coord c) {
+            queue<coord> queue;
+            queue.push(c);
+            while (not queue.empty()) {
+                int c0 = queue.front();
+                queue.pop();
+                if (not admit[c0])
+                    throw NO_SOLUTION_EXCEPTION;
+                for (block b1 : parents[c0]) {
+                    count[b1] = DEFINED;
+                    for (coord c2 : children[b1]) if (c2 != c0 and admit[c2]) {
+                        admit[c2] = false;
+                        for (block b3 : parents[c2]) if (b3 != b1) {
+                            count[b3]--;
+                            if (count[b3] == 0)
+                                throw NO_SOLUTION_EXCEPTION;
+                            if (count[b3] == 1)
+                                for (coord c4 : children[b3]) if (admit[c4])
+                                    queue.push(c4);
                         }
-                        output += char_of(k);
                     }
-                    else {
+                }
+            }
+            return *this;
+        }
+
+        string to_string() {
+            string output;
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    vector<int> ks;
+                    for (int k = 0; k < SIZE; k++)
+                        if (admit[coord_of(i, j, k)])
+                            ks.push_back(k);
+                    if (ks.size() == 1)
+                        output += DIGITS[ks[0]];
+                    else
                         output += '.';
-                    }
                 }
             }
             return output;
@@ -197,19 +167,9 @@ class sudoku {
 };
 
 int main() {
-    initialize();
+    init();
     string input;
-    sudoku s;
-    while (true) {
-        cin >> input;
-        if (cin.eof()) {
-            return 0;
-        }
-        try {
-            cout << s.clear().read(input).solve().show() << endl;
-        }
-        catch (no_solution) {
-            cout << "no solution" << endl;
-        }
-    }
+    while (cin >> input)
+        cout << sudoku::solve(input) << endl;
+    return 0;
 }
