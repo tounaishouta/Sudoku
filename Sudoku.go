743548d82c53552c38d1a2e769dbb00f8ffad0d6
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-10-31 20:02:38 +0900

    modify

--- Sudoku.go
+++ Sudoku.go
@@ -34,11 +34,11 @@ const (
 	nBlock = view * size * size
 	nCoord = size * size * size
 
-	defined = size + 1
+	done = size + 1
 
-	possible   = 0
-	impossible = 1
-	fixed      = 2
+	open   = 0
+	fixed  = 1
+	banned = 2
 
 	digits = "123456789"
 )
@@ -47,9 +47,9 @@ type (
 	block  int
 	coord  int
 	sudoku struct {
-		bstate [nBlock]int
-		cstate [nCoord]int
-		queue  []coord
+		state [nCoord]int
+		count [nBlock]int
+		queue []coord
 	}
 )
 
@@ -89,22 +89,40 @@ func init() {
 
 func NewSudoku() *sudoku {
 	s := new(sudoku)
-	for b := range s.bstate {
-		s.bstate[b] = size
+	for b := range s.count {
+		s.count[b] = size
 	}
-	for c := range s.cstate {
-		s.cstate[c] = possible
+	for c := range s.state {
+		s.state[c] = open
 	}
+	return s
+}
+
+func (s *sudoku) Clone() *sudoku {
+	ss := new(sudoku)
+	for b, v := range s.count {
+		ss.count[b] = v
+	}
+	for c, v := range s.state {
+		ss.state[c] = v
+	}
+	return ss
+}
+
+func (s *sudoku) Fix(c coord) *sudoku {
+	s.queue = append(s.queue, c)
 	return s
 }
 
 func (s *sudoku) Read(input string) *sudoku {
 	for ij, r := range input {
-		if ij >= size*size {
-			break
-		}
-		if k := strings.IndexRune(digits, r); k != -1 {
-			s.Assign(coordOf(ij/size, ij%size, k))
+		if ij < size*size {
+			i := ij / size
+			j := ij % size
+			k := strings.IndexRune(digits, r)
+			if k != -1 {
+				s.Fix(coordOf(i, j, k))
+			}
 		}
 	}
 	return s
@@ -115,28 +133,28 @@ func (s *sudoku) Search() (*sudoku, bool) {
 	for len(s.queue) > 0 {
 		c0 := s.queue[0]
 		s.queue = s.queue[1:]
-		if s.cstate[c0] == fixed {
+		if s.state[c0] == fixed {
 			continue
 		}
-		if s.cstate[c0] == impossible {
+		if s.state[c0] == banned {
 			return nil, false
 		}
-		s.cstate[c0] = fixed
+		s.state[c0] = fixed
 		for _, b1 := range parents[c0] {
-			s.bstate[b1] = defined
+			s.count[b1] = done
 			for _, c2 := range children[b1] {
-				if c2 != c0 && s.cstate[c2] != impossible {
-					s.cstate[c2] = impossible
+				if c2 != c0 && s.state[c2] != banned {
+					s.state[c2] = banned
 					for _, b3 := range parents[c2] {
 						if b3 != b1 {
-							s.bstate[b3]--
-							if s.bstate[b3] == 0 {
+							s.count[b3]--
+							if s.count[b3] == 0 {
 								return nil, false
 							}
-							if s.bstate[b3] == 1 {
+							if s.count[b3] == 1 {
 								for _, c4 := range children[b3] {
-									if s.cstate[c4] == possible {
-										s.Assign(c4)
+									if s.state[c4] == open {
+										s.Fix(c4)
 									}
 								}
 							}
@@ -145,23 +163,22 @@ func (s *sudoku) Search() (*sudoku, bool) {
 				}
 			}
 		}
-
 	}
 
-	min, bmin := defined, -1
-	for b, v := range s.bstate {
+	min, bmin := done, -1
+	for b, v := range s.count {
 		if v < min {
 			min, bmin = v, b
 		}
 	}
 
-	if min == defined {
+	if min == done {
 		return s, true
 	}
 
 	for _, c := range children[bmin] {
-		if s.cstate[c] == possible {
-			if ss, ok := s.Clone().Assign(c).Search(); ok {
+		if s.state[c] == open {
+			if ss, ok := s.Clone().Fix(c).Search(); ok {
 				return ss, true
 			}
 		}
@@ -170,29 +187,13 @@ func (s *sudoku) Search() (*sudoku, bool) {
 	return nil, false
 }
 
-func (s *sudoku) Assign(c coord) *sudoku {
-	s.queue = append(s.queue, c)
-	return s
-}
-
-func (s *sudoku) Clone() *sudoku {
-	ss := new(sudoku)
-	for b, v := range s.bstate {
-		ss.bstate[b] = v
-	}
-	for c, v := range s.cstate {
-		ss.cstate[c] = v
-	}
-	return ss
-}
-
 func (s *sudoku) String() string {
 	output := make([]rune, size*size)
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			output[i*size+j] = '.'
 			for k := 0; k < size; k++ {
-				if s.cstate[coordOf(i, j, k)] == fixed {
+				if s.state[coordOf(i, j, k)] == fixed {
 					output[i*size+j] = rune(digits[k])
 				}
 			}
