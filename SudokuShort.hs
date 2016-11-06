3f6749f25ae3df29490ab84253d2099b49ae1771
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-11-07 01:50:26 +0900

    modify

--- SudokuShort.hs
+++ SudokuShort.hs
@@ -7,19 +7,22 @@ main = interact $ unlines . map (showSudoku . head . solutions . readSudoku) . l
 type Sudoku = UArray (Int, Int) Char
 
 solutions :: Sudoku -> [Sudoku]
-solutions s = case [ p | p <- range bds, s ! p == '.' ] of
+solutions s = case [ p | p <- range table, s ! p `notElem` digits ] of
                 []  -> [s]
                 p:_ -> concatMap solutions [ s // [(p, d)] | d <- ds ]
-                    where ds = "123456789" \\ [ s ! q | q <- range bds, p ~~ q ]
+                    where ds = digits \\ [ s ! q | q <- range table, p ~~ q ]
 
 (~~) :: (Int, Int) -> (Int, Int) -> Bool
 (x, y) ~~ (z, w) = x == z || y == w || (x`div`3 == z`div`3 && y`div`3 == w`div`3)
 
-bds :: ((Int, Int), (Int, Int))
-bds = ((0, 0), (8, 8))
+digits :: String
+digits = "123456789"
+
+table :: ((Int, Int), (Int, Int))
+table = ((0, 0), (8, 8))
 
 readSudoku :: String -> Sudoku
-readSudoku = listArray bds
+readSudoku = listArray table
 
 showSudoku :: Sudoku -> String
 showSudoku = elems
